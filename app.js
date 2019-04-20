48a1c9c999dab5b47b531b6840c6a0923c541f3d
Mahdihrs <mahdiharis@outlook.com>
2019-04-20 18:53:17 +0700

    cors added

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 
 const app = express();
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
